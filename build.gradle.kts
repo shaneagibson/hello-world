14151964dad4cdeeb77d068f5d1d07dd1eb3cbd7
Shane Gibson <shane.a.gibson@gmail.com>
2026-02-20 11:23:09 +0100

    Fix: upgrade java and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "2.3.10"
     kotlin("plugin.spring") version "1.9.22"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.5"
