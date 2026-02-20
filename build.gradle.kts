dc141f2c9bab85da269dedb13d1cb317da0383c9
Shane Gibson <shane.a.gibson@gmail.com>
2026-02-20 11:18:47 +0100

    Fix: upgrade java and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_25
 }
 
 repositories {
@@ -45,7 +45,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "21"
+        jvmTarget = "25"
     }
 }
 
